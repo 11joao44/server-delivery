566348ddd073014f041ef9454ef2c186d87816a1
João Pedro Inácio Ferreira <11joao44@gmail.com>
2024-09-29 19:33:52 -0400

    Fix: correção cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['https://graceful-manifestation-production.up.railway.app'],
+    origin: 'https://graceful-manifestation-production.up.railway.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
