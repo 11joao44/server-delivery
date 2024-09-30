f5dd9124d2bc4dfdd2233bfd72228ec8120de689
João Pedro Inácio Ferreira <11joao44@gmail.com>
2024-09-29 21:17:40 -0400

    Updatate: ajustando rota home

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(cors({
     credentials: true
 }));
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 8000;
 
